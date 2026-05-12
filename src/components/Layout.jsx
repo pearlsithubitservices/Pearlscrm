e947efa16774f222455611c436d24244224b1bef
Ragavi Muthuraman <ragavim07072003@gmail.com>
2026-05-12 06:55:57 +0530

    6th

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import Sidebar from './sidebar';
 import { useAuth } from '../context/AuthContext';
 import { motion, AnimatePresence } from 'framer-motion';
 
