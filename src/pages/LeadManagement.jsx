a12beaa92e77e0f4df05d8f65c2c685f898ecef3
Deepan0314 <deepanraj0314@gmail.com>
2026-06-29 13:04:17 +0530

    leads end point

--- src/pages/LeadManagement.jsx
+++ src/pages/LeadManagement.jsx
@@ -78,7 +78,7 @@ export default function LeadManagement() {
 
         const response =
           await fetch(
-            "http://localhost:5000/api/leads"
+            "https://pearlscrm.onrender.com/api/leads"
           );
 
         const data =
