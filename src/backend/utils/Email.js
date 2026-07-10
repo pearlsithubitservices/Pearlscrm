659743a4f4cc44ee475f64889421e2b04ce2f493
Deepan0314 <deepanraj0314@gmail.com>
2026-07-10 12:29:03 +0530

    update 1

--- src/backend/utils/Email.js
+++ src/backend/utils/Email.js
@@ -1,15 +1,28 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true only for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Verify Error:", error);
+  } else {
+    console.log("SMTP Server is ready");
+  }
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-    
+
+  console.log("Starting sendMail...");
   console.log("Sending email to:", to);
 
   const info = await transporter.sendMail({
@@ -19,9 +32,9 @@ const sendEmail = async ({ to, subject, html }) => {
     html,
   });
 
-//   console.log("Email sent successfully!");
-//   console.log(info.response);
-//   console.log(info.messageId);
+  //   console.log("Email sent successfully!");
+  //   console.log(info.response);
+  //   console.log(info.messageId);
 };
 
 module.exports = { sendEmail };
